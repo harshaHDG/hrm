d1ddc130d95410ba889481e1f8cefe30e18c9537
HARSHA HD <HARSHA HD@DESKTOP-JF342SR>
2022-09-27 12:47:45 +0530

    first commit

---
+++ src/test/java/github_package/create_test.java
@@ -0,0 +1,12 @@
+package github_package;
+
+import org.testng.annotations.Test;
+
+public class create_test {
+	@Test
+	public void create()
+	{
+		System.out.println("===========created==========");
+	}
+
+}
